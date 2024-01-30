025d531451cdff0ddc2e40913cbf089b46bfc039
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2024-01-30 15:24:54 +0200

    Delete comments

--- src/get_coordinates.js
+++ src/get_coordinates.js
@@ -3,15 +3,10 @@ export default function getCoordinates() {
   const startY = Math.floor(Math.random() * 8) + 0;
   const startPoint = [startX, startY];
 
-  // for testing
-  // const startPoint = [3, 2]
-
   let endX = Math.floor(Math.random() * 8) + 0;
   let endY = Math.floor(Math.random() * 8) + 0;
   let endPoint = [endX, endY];
 
-  // for testing
-  // let endPoint = [5, 1]
  
   // if startPoint==endPoint , generate new endPoint
   while ( JSON.stringify(startPoint)==JSON.stringify(endPoint) ) {
